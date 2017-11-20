04ba620508654c6976a0b196dc46e035008b8331
jjadh579 <Jayesh.Jadhav@marriott-sp.com>
2017-11-20 14:01:24 +0530

    initial commit

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -24,6 +24,20 @@ public class HomeController extends Controller {
     public CompletionStage<Result> index() {
         WSRequest request = ws.url("https://jsonplaceholder.typicode.com/posts/1");
 
+        /* More API @ https://www.playframework.com/documentation/2.6.x/JavaWS
+        request.addQueryParameter("paramKey", "paramValue");
+        request.addHeader("headerKey", "headerValue");
+        request.setContentType("application/json");
+        request.addCookies(new WSCookieBuilder().setName("headerKey").setValue("headerValue").build());
+        request.setRequestTimeout(Duration.of(1000, ChronoUnit.MILLIS));
+        request.post("key1=value1&key2=value2"); // Submitting form data
+        */
+
+        JsonNode json = Json.newObject()
+                .put("key1", "value1")
+                .put("key2", "value2");
+        request.post(json);
+
         return request.get().thenApply((WSResponse r) -> {
             return ok("Feed title: " + r.getBody());
         });
